e04e34f6fe6e8e298a41b5ec559929c1babf48fa
theInevitable007 <beingadithya06@gmail.com>
2020-06-19 18:46:28 +0530

    commit

--- sketch.js
+++ sketch.js
@@ -1,10 +1,14 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
+var chain;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+
+
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -15,6 +19,7 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
+ 
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
@@ -29,6 +34,7 @@ function setup(){
     pig3 = new Pig(810, 220);
 
     log3 =  new Log(810,180,300, PI/2);
+    log6 = new Log(230,180,80,PI/2);
 
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
@@ -36,6 +42,12 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    chain = new Constraints(bird.body,log6.body);
+
+    
+
+   
+    
 }
 
 function draw(){
@@ -58,7 +70,14 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    log6.display();
 
     bird.display();
     platform.display();
+
+    chain.display();
+
+    
+    
+    
 }
